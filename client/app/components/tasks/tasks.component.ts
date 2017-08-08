cfcb55cc6a0293d6c9ebb325445ef156421966e1
Saksham Ghimire <saksham.ghimire@charter.com>
2017-08-07 18:33:34 -0600

    adding all deps and files

---
+++ client/app/components/tasks/tasks.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {TaskService} from '../../services/task.service';
+import {Task} from '../../../Task';
+
+@Component({
+    moduleId: module.id,
+    selector: 'tasks',
+    templateUrl: 'tasks.component.html'
+})
+
+export class TasksComponent {
+    tasks: Task[];
+    title: string;
+
+    constructor(private taskService:TaskService){
+        this.taskService.getTasks().subscribe(tasks => {
+            this.tasks = tasks;
+        });
+    }
+
+    addTask(event){
+        event.preventDefault();
+        var newTask = {
+            title: this.title,
+            isDone: false
+        }
+        this.taskService.addTask(newTask)
+            .subscribe(
+                task => {
+                    this.tasks.push(task);
+                    this.title = '';
+                }
+            )
+    }
+}
---
+++ client/app/services/task.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class TaskService {
+    constructor(private http:Http){
+        console.log('Task Service INitialized ...');
+    }
+
+
+    getTasks(){
+        return this.http.get('http://localhost:3000/api/tasks')
+            .map(res => res.json());
+    }
+
+    addTask(newTask){
+        console.log(newTask);
+        var headers = new Headers();
+        headers.append('Content-type', 'application/json');
+        return this.http.post('http://localhost:3000/api/task', JSON.stringify(newTask), {headers: headers})
+            .map (res => res.json());
+
+    }
+}
